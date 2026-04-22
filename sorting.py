bd3a0c62592b2a609e0a46662d850e605f377251
Jitka Dvořáková <283160@vutbr.cz>
2026-04-22 09:43:37 +0200

    sorting, bubble done

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -16,10 +17,37 @@ def selection_sort(numbers):
     return copy_random_num
 
 
+def bubble_sort(numbers):
+    plt.ion()
+    plt.show()
+    numbers_copy = numbers.copy()
+    n = len(numbers_copy)
+    for num in range(n-1):
+        for idx in range(n-1-num):
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+            if numbers_copy[num] > numbers_copy[num+1]:
+                numbers_copy[num], numbers_copy[num+1] = numbers_copy[num+1], numbers_copy[num]
+    plt.ioff()
+    plt.show()
+
+    return numbers_copy
+
+
 def main():
     sequence = random_numbers(10, 0, 100)
-    sorted = selection_sort(sequence)
-    print(sorted)
+    sorted1 = selection_sort(sequence)
+    sorted2 = bubble_sort(sequence)
+    print(sorted1)
+    print(sorted2)
+
 
 
 if __name__ == "__main__":
